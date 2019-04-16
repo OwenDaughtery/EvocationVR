7055d19eb9a085f0e149c6c31e4e198c97685f6a
Owen Daughtery <OwenJ.daughtery@gmail.com>
2019-04-16 09:32:34 +0100

    FPS Controller, Object Pool, testspell spawner

---
+++ EvocationVR/Assets/Scripts/SpellManager.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpellManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        anyKeyPressed();
+    }
+
+    private void anyKeyPressed() {
+        if (Input.GetKeyDown("e"))
+        {
+            shootSpell();
+        }
+    }
+
+    private void shootSpell()
+    {
+        
+        GameObject testSpell = ObjectPoolerManager.SharedInstance.GetPooledObject("TestSpell");
+        if (testSpell != null)
+        {
+            //set position of bullet
+            testSpell.transform.position = this.transform.position;
+            testSpell.SetActive(true);
+
+
+        }
+
+    }
+}
