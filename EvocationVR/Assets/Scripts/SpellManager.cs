5f36971874575c3dadd99f09c3931797cecb4e42
Owen Daughtery <OwenJ.daughtery@gmail.com>
2019-04-23 20:09:30 +0100

    fix ghost testspell
    the extra frame for the testspell was causing problems when deactivating and then hitting an enemy.

--- EvocationVR/Assets/Scripts/SpellManager.cs
+++ EvocationVR/Assets/Scripts/SpellManager.cs
@@ -67,20 +67,25 @@ public class SpellManager : MonoBehaviour
 
     IEnumerator collided(){
         yield return new WaitForEndOfFrame();
-        gameObject.GetComponent<MeshRenderer>().enabled = false;
-        int numberOfChildren = this.transform.childCount;
-        for (int i = 0; i < numberOfChildren; i++)
-        {
-            GameObject child = this.transform.GetChild(i).gameObject;
-            child.SetActive(false);
-        }
-        yield return new WaitForEndOfFrame();
-        ObjectPoolerManager.SharedInstance.returnToPool(this.gameObject, "TestSpell");
-        gameObject.GetComponent<MeshRenderer>().enabled = true;
-        for (int i = 0; i < numberOfChildren; i++)
-        {
-            GameObject child = this.transform.GetChild(i).gameObject;
-            child.SetActive(true);
+        if (gameObject.GetComponent<MeshRenderer>().enabled) {
+
+        
+            gameObject.GetComponent<MeshRenderer>().enabled = false;
+            int numberOfChildren = this.transform.childCount;
+            for (int i = 0; i < numberOfChildren; i++)
+            {
+                GameObject child = this.transform.GetChild(i).gameObject;
+                child.SetActive(false);
+            }
+            yield return new WaitForEndOfFrame();
+            ObjectPoolerManager.SharedInstance.returnToPool(this.gameObject, "TestSpell");
+            gameObject.GetComponent<MeshRenderer>().enabled = true;
+            for (int i = 0; i < numberOfChildren; i++)
+            {
+                GameObject child = this.transform.GetChild(i).gameObject;
+                child.SetActive(true);
+            }
+            
         }
         StopCoroutine(collided());
     }
